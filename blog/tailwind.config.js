1899fc52df1c1871c43d151688f07a8c93ba84ff
CanhVo <canh.vo@asnet.com.vn>
2023-10-23 17:29:10 +0700

    Update tailwind.config

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -2,22 +2,17 @@ import theme from './src/themes'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
-    fontFamily: {
-      primary: ['var(--work-sans-font)'],
-      secondary: ['var(--source-serif-pro-font)'],
-      tertiary: ['var(--plus-jakarta-sans-font)'],
-      quaternary: ['var(--inter-font)'],
+    extend: {
+      fontFamily: {
+        primary: ['var(--work-sans-font)'],
+        secondary: ['var(--source-serif-pro-font)'],
+        tertiary: ['var(--plus-jakarta-sans-font)'],
+        quaternary: ['var(--inter-font)'],
+      },
     },
+
     ...theme,
   },
   plugins: [],
