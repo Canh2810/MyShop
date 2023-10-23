975be338cc7b7d4d06ee5eec885d0bfe451a7e74
CanhVo <canh.vo@asnet.com.vn>
2023-10-23 15:41:08 +0700

    Remove Chakra UI, instal tailwind, define theme

--- blog/src/app/providers.tsx
+++ blog/src/app/providers.tsx
@@ -1,19 +1,14 @@
-// app/providers.tsx
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from '@/themes'
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
 
 const queryClient = new QueryClient()
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <CacheProvider>
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
-      </CacheProvider>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
     </QueryClientProvider>
   )
 }
