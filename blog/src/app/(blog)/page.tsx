0905de634652690f2b96e00d97cf9ccea8099c56
CanhVo <canh.vo@asnet.com.vn>
2023-10-24 15:45:12 +0700

    Set up color mode

--- blog/src/app/(blog)/page.tsx
+++ blog/src/app/(blog)/page.tsx
@@ -1,12 +1,22 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+
 const Home = () => {
+  const { resolvedTheme, setTheme } = useTheme()
   return (
     <>
-      <h1 className="font-primary text-primary text-2xl leading-8">
-        This is text
-      </h1>
+      <h1 className="font-primary text-2xl leading-8">This is text</h1>
       <h1 className="font-secondary text-light-700">This is text</h1>
       <h1 className="font-tertiary">This is text</h1>
       <h1 className="font-quaternary">This is text</h1>
+      <button
+        onClick={() => {
+          setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+        }}
+      >
+        Toggle
+      </button>
     </>
   )
 }
