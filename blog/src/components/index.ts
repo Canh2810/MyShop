ab6aed2ec76fd4eab123df0a37a83a2b77cbbe52
CanhVo <canh.vo@asnet.com.vn>
2023-10-25 10:03:01 +0700

    Update and write unit testing for button component

--- blog/src/components/index.ts
+++ blog/src/components/index.ts
@@ -1,2 +1,2 @@
-export { default as Typography } from '@/components/Typography'
-export { default as Button } from '@/components/Button'
+export { default as Typography } from './Typography'
+export { default as Button } from './Button'
