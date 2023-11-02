c0d7421dbba1be5e992400aac842f329572e2f85
CanhVo <canh.vo@asnet.com.vn>
2023-11-02 10:47:48 +0700

    Navigate to home page when click logo

--- blog/src/components/Header/index.tsx
+++ blog/src/components/Header/index.tsx
@@ -3,6 +3,7 @@
 // Libs
 import { useCallback, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 // Assets
 import { ChevronDown, Logo, Search } from '@/assets'
@@ -41,13 +42,17 @@ const Header = () => {
 
   // Handle logout
   const handleLogout = useCallback(() => {
+    // Clear token in local storage
     localStorage.removeItem(LOCAL_STORAGE_KEY.AUTH)
+    // Navigate to login page
     router.push(ROUTES.LOGIN)
   }, [router])
 
   return (
     <div className="py-8 max-w-[1216px] mx-auto flex items-center justify-between fixed top-0 left-0 right-0 z-50 bg-white dark:bg-dark-300">
-      <Logo />
+      <Link href={ROUTES.HOME}>
+        <Logo />
+      </Link>
       <Menu listItems={HEADER_MENU} />
       <div className="relative flex items-center gap-4">
         <TextField
